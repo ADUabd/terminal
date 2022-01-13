d95f2c2975a0629bbd981f2057e0b5a04616a882
ADUabd <97505497+ADUabd@users.noreply.github.com>
2022-01-13 11:01:08 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/gitihubik/MainActivity.java
+++ app/src/main/java/com/example/gitihubik/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ZUJNMSKIAFN;;;;;JNNNNNNNNNNNNNNNNNNNNNNFFFFFFFFFFFFFFFF
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
